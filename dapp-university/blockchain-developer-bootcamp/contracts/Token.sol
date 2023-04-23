ed4abc8eb98622a1c332e2d26a61c2024dcb0033
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2023-04-24 08:41:29 +1000

    adds constructor, refactor tests, abstracts params to variables

--- dapp-university/blockchain-developer-bootcamp/contracts/Token.sol
+++ dapp-university/blockchain-developer-bootcamp/contracts/Token.sol
@@ -4,9 +4,19 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract Token {
-    //manually coding out to ERC-20 standard at https://ethereum.org/en/developers/docs/standards/tokens/erc-20/
-    string public name = "Dapp University";
-    string public symbol = "DAPP";
+    // manually coding out to ERC-20 standard at 
+    // https://ethereum.org/en/developers/docs/standards/tokens/erc-20/
+    string public name;
+    string public symbol;
     uint256 public decimals = 18;
-    uint256 public totalSupply = 1000000 * (10**decimals);
+    uint256 public totalSupply;
+    
+    constructor(string memory _name, 
+                string memory _symbol, 
+                uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals);
+
+    }
 }
--- dapp-university/blockchain-developer-bootcamp/test/Token.js
+++ dapp-university/blockchain-developer-bootcamp/test/Token.js
@@ -1,36 +1,41 @@
-const {ethers} = require('hardhat');
-const {expect} = require('chai');
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
 describe("Token", async () => {
-    let token;
+  let token;
 
-    beforeEach(async () => {
-        // Code goes here that runs before each example
-        
-        //Fetch token from blockchain
-        const Token = await ethers.getContractFactory("Token");
-        token = await Token.deploy();
-    });
+  beforeEach(async () => {
+    // Code goes here that runs before each example
 
-    //Tests go inside here...
+    //Fetch token from blockchain
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy("Dapp University", "DAPP", 1000000);
+  });
+  describe("Deployment", async () => {
+    const name = "Dapp University";
+    const symbol = "DAPP";
+    const decimals = 18;
+    const totalSupply = 1000000;
     it("has the correct name", async () => {
-        //Check that the name is correct
-        expect(await token.name()).to.equal("Dapp University");
+      //Check that the name is correct
+      expect(await token.name()).to.equal(name);
     });
 
     it("has the correct symbol", async () => {
-        //Check that the symbol is correct
-        expect(await token.symbol()).to.equal("DAPP");
+      //Check that the symbol is correct
+      expect(await token.symbol()).to.equal(symbol);
     });
 
     it("has the correct decimals", async () => {
-        //Check that the decimals is correct
-        expect(await token.decimals()).to.equal(18);
+      //Check that the decimals is correct
+      expect(await token.decimals()).to.equal(decimals);
     });
 
     it("has the correct total supply", async () => {
-        //Check that the total supply is correct
-        expect(await token.totalSupply()).to.equal(ethers.utils.parseEther("1000000"));
+      //Check that the total supply is correct
+      expect(await token.totalSupply()).to.equal(
+        ethers.utils.parseEther(totalSupply.toString())
+      );
     });
-
+  });
 });
