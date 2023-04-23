a619403328ec3fddaddf6bd8ed14108cc8e938fc
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2023-04-24 08:26:29 +1000

    adds basic tests and properties for a simple token smart contract

--- dapp-university/blockchain-developer-bootcamp/contracts/Token.sol
+++ dapp-university/blockchain-developer-bootcamp/contracts/Token.sol
@@ -7,4 +7,6 @@ contract Token {
     //manually coding out to ERC-20 standard at https://ethereum.org/en/developers/docs/standards/tokens/erc-20/
     string public name = "Dapp University";
     string public symbol = "DAPP";
+    uint256 public decimals = 18;
+    uint256 public totalSupply = 1000000 * (10**decimals);
 }
--- dapp-university/blockchain-developer-bootcamp/test/Token.js
+++ dapp-university/blockchain-developer-bootcamp/test/Token.js
@@ -14,22 +14,23 @@ describe("Token", async () => {
 
     //Tests go inside here...
     it("has the correct name", async () => {
-        //Check that name is correct
-
-
-        //Read token name
-        const name = await token.name();
         //Check that the name is correct
-        expect(name).to.equal("Dapp University");
+        expect(await token.name()).to.equal("Dapp University");
     });
 
     it("has the correct symbol", async () => {
-        //Check that symbol is correct
-
-        //Read token symbol
-        const symbol = await token.symbol();
         //Check that the symbol is correct
-        expect(symbol).to.equal("DAPP");
+        expect(await token.symbol()).to.equal("DAPP");
+    });
+
+    it("has the correct decimals", async () => {
+        //Check that the decimals is correct
+        expect(await token.decimals()).to.equal(18);
+    });
+
+    it("has the correct total supply", async () => {
+        //Check that the total supply is correct
+        expect(await token.totalSupply()).to.equal(ethers.utils.parseEther("1000000"));
     });
 
 });
